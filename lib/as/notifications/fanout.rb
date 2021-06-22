dd59bf19eddd2fad829eff1cb442953569db7a9a
Bernd Ahlers <bernd@users.noreply.github.com>
2021-06-22 19:40:09 +0200

    Make AS::Notifications::Fanout#subscribe work with Ruby 3.0 (#3)
    * Add Ruby 3.0 to workflow test matrix
    * Rename callback to callable to match upstream

--- lib/as/notifications/fanout.rb
+++ lib/as/notifications/fanout.rb
@@ -15,8 +15,8 @@ module AS
         super
       end
 
-      def subscribe(pattern = nil, block = Proc.new)
-        subscriber = Subscribers.new pattern, block
+      def subscribe(pattern = nil, callable = nil, &block)
+        subscriber = Subscribers.new pattern, callable || block
         synchronize do
           @subscribers << subscriber
           @listeners_for.clear
